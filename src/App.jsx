e5136ce62dd1c90c9affc87809651b27d4bd2b89
Durvesh More <durubhuru01@gmail.com>
2025-04-15 20:32:18 +0530

    Changed max width of main to fixed (1280px)

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div className="bg-pink-400 h-dvh w-dvw flex justify-center items-center flex-col relative">
-      <main className="h-[90%] w-[80%] bg-white rounded-[50px] min-w-[320px] max-h-[700px] max-w-[800px] shadow-(--clay-pink-shadow-body) p-8 flex justify-center items-center flex-col gap-y-5 xl:flex-row xl:justify-around xl:max-w-4/6 xl:max-h-5/6">
+      <main className="h-[90%] w-[80%] bg-white rounded-[50px] min-w-[320px] max-h-[700px] max-w-[800px] shadow-(--clay-pink-shadow-body) p-8 flex justify-center items-center flex-col gap-y-5 xl:flex-row xl:justify-around xl:w-4/6 xl:max-w-[1280px]">
         <Profile />
         <div className="flex justify-center content-center flex-col gap-y-3">
           <Bio />
